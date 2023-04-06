3612cb163e85a8e97cbf7769d6b454b6f888577a
hodei <Hodeione41@gmail.com>
2023-04-06 17:20:30 +0100

    Ejercicio3

---
+++ src/main/java/Ejercicio123/Ejercicio3.java
@@ -0,0 +1,21 @@
+package Ejercicio123;
+
+public class Ejercicio3 {
+    //¿Qué es la visibilidad de una variable o un método? ¿Qué tipos hay? ¿Cuándo debe utilizarse cada
+    //tipo?
+
+    //La visibilidad en la programación orientada a objetos se refiere a la capacidad de acceder a los atributos y métodos de una clase desde otras partes del código.
+    // En general, existen tres niveles de visibilidad que se pueden asignar a los atributos y métodos de una clase:
+    //
+    //Público (public): los atributos y métodos públicos son accesibles desde cualquier parte del código, incluso desde otras clases y paquetes.
+    // Este nivel de visibilidad se utiliza para los elementos que deben ser utilizados por cualquier otra parte del código, y que no
+    // necesitan ser protegidos o encapsulados.
+    //
+    //Privado (private): los atributos y métodos privados solo son accesibles desde la propia clase que los define.
+    // Este nivel de visibilidad se utiliza para proteger los atributos y métodos internos de la clase, evitando que
+    // se modifiquen o accedan de forma inapropiada desde otras partes del código.
+    //
+    //Protegido (protected): los atributos y métodos protegidos son accesibles desde la propia clase y sus subclases.
+    // Este nivel de visibilidad se utiliza cuando se desea proteger los atributos y métodos internos de una clase,
+    // pero permitiendo que las subclases los hereden y los modifiquen según sus necesidades.
+}
