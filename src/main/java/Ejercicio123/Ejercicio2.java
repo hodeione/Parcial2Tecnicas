88019acb904383e2bd29c33ebda21c0fd6f23a7f
hodei <Hodeione41@gmail.com>
2023-04-06 17:10:04 +0100

    Ejercicio2

--- src/main/java/Ejercicio123/Ejercicio2.java
+++ src/main/java/Ejercicio123/Ejercicio2.java
@@ -2,4 +2,14 @@ package Ejercicio123;
 
 public class Ejercicio2 {
 
+    //La programacion orientada a objetos (POO) es un tipo de programacion que se basa en la creacion
+    //y manipulación de objetos. Los objetos serán representaciones de entidades o conceptos del mundo real
+    //,estos poseerán atributps que describiran el estado y metodos que definirán su comportamiento
+
+    // POO se caracterizará por cinco pilares:
+    //      *la abstracción permite enfocarse en los aspectos importantes de un objet.
+    //      *la encapsulación permite ocultar los detalles de implementación de un objeto.
+    //      *la herencia permite definir una nueva clase a partir de una clase existente.
+    //      *el polimorfismo permite definir una misma operación para diferentes tipos de objetos.
+    //      *las clases y objetos son los elementos básicos de la POO.
 }
