bb17eccf75fa15a5a0e8b2b2705312c75eacc30f
hodei <Hodeione41@gmail.com>
2023-04-06 16:57:13 +0100

    Ejercicio1

---
+++ src/main/java/Ejercicio123/Ejercicio1.java
@@ -0,0 +1,43 @@
+package Ejercicio123;
+
+public class Ejercicio1 {
+
+//1) Los elementos que definen a un objeto son:
+//a) El tipo de visibilidad que tiene asignado: privado o público.
+//b) La identidad, Los atributos que representan su estado y los métodos que representan su
+//comportamiento.
+
+//La respuesta correcta es la b
+
+
+//2) El hecho de que un método pueda recibir un número variable de atributos se denomina:
+//a) Herencia
+//b) Polimorfismo
+
+//La respuesta correcta es la b
+
+
+//3) Imagine que la clase C hereda de la clase B. Y la clase B hereda de la clase A. Cuando se cree
+//un objeto de la clase C, ¿cuál será la secuencia en que se ejecutarán los constructores?
+//A) Primero el constructor de C, luego constructor de B y, finalmente, el constructor de A
+//b) Primero el constructor de A, luego constructor de B y, finalmente, el constructor de C
+
+//La respuesta correcta es la a
+
+
+//4) Para referenciar los atributos de un objeto en un método de la misma clase
+//a) Debo utilizar la palabra reservada self como prefijo
+//b) Los atributos deben ser públicos, en caso contrario no se pueden referenciar desde
+//métodos que no sean el constructor.étodos que no sean el constructor.étodos que no
+//sean el constructor.
+
+//La respuesta correcta es la b
+
+
+//5) En el paradigma de la programación orientado a objetos:
+//a) Se recomienda que las acciones estén lo más acopladas posibles al código para
+//obtener el mayor rendimiento posible.
+//b) Se favorece la semántica y la reutilización de código en detrimento del rendimiento.
+
+//La respuesta correcta es la b
+}
